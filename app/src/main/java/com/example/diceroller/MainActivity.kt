2f42fe08927f137745a74269998b713fd20705b2
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2022-02-09 13:21:17 -0600

    practice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -46,6 +46,28 @@ class MainActivity : AppCompatActivity() {
 
         // Update the content description
         diceImage.contentDescription = diceRoll.toString()
+
+        val dice2 = Dice(6)
+        val diceRoll2 = dice2.roll()
+
+        // Find the ImageView in the layout
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource2 = when(diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage2.setImageResource(drawableResource2)
+
+        // Update the content description
+        diceImage2.contentDescription = diceRoll2.toString()
     }
 }
 
